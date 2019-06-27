5a729f27a0306baf12b6c15737c992ba3b912dcb
Anthony Zardis <anthonyzardis@gmail.com>
2019-06-27 15:45:12 -0700

    auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../model');
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
+});
+router.post('/signup', function(req, res) {
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      console.log('User created!')
+      res.redirect('/');
+    } else {
+      console.log('Email already exists!');
+      res.redirect('/auth/signup');
+    }
+  }).catch(function(error) {
+    console.log('An error occured: ' + error.message);
+    res.redirect('/auth/signup');
+  });
 });
 
 router.get('/login', function(req, res) {
