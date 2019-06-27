cdf9061fb3955e05951c4b425b9b4bd96bad1737
Anthony Zardis <anthonyzardis@gmail.com>
2019-06-27 16:09:06 -0700

    session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../model');
+const db = require('../models');
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
