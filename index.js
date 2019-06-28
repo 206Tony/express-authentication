ce3e2609ca30489cdf88167b054f8c1f1a25829e
Anthony Zardis <anthonyzardis@gmail.com>
2019-06-28 12:30:51 -0700

    finished auth code along

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const passport = require('./config/passportConfig');
 //modules for flash messages
 const flash = require('connect-flash');
 const isLoggedIn = require('./middleware/isLoggedIn');
+const helmet = require('helmet');
+//this is only used by session store
+const db = require('./models');
+
 const app = express();
+
+//This line makes the session use sequelize to write session data to postgres table
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sessionStore = new SequelizeStore({
+  db: db.sequelize, 
+  expiration: 1000 * 60 * 30
+});
 
 app.set('view engine', 'ejs');
 
@@ -16,13 +28,18 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
+app.use(helmet());
 
 //Configures the express-session middleware
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: sessionStore
 }));
+
+// use this line once to setup the store table
+sessionStore.sync();
 
 // Starts the flash middleware
 app.use(flash());
